649a03b08877b591b52f476121283382834dc6f6
Huzaifa Nagariya <huzaifaali578@gmail.com>
2024-09-12 19:44:03 +0530

    Adding feature

--- backend/middleware/cvs_fileUploader.middleware.js
+++ backend/middleware/cvs_fileUploader.middleware.js
@@ -1,6 +1,11 @@
 import multer from "multer";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+// Create __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadDir = path.join(__dirname, 'uploads');
 
